4a80fc16643304ac07f49760c274be6257b923c8
Kaic Melo <kaicmelosantos@gmail.com>
2020-04-03 01:17:56 -0300

    Fazendo as separação dos componentes em arquivos externos para melhora a visibilidade no main do sistema! Foi iimplementado novos recursos nos modulos

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,59 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import './perguntas.dart';
+import './resposta.dart';
 
 main() => runApp(new PerguntaApp());
-  void responder()
-  {
-    print("Responder Prguinta");
+
+class _perguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
   }
 
-class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final List<String> pergunta =
-    [
-      "Qual sua cor favorita ?",
-      "Qual seu animal favorito ?",
+    final List<Map<String, Object>> pergunta = [
+      {
+        'texto': "Qual sua cor favorita ?",
+        'respostas': ['Preto', 'Vermelhor', 'Verde', 'Branco'],
+      },
+      {
+        'texto': "Qual seu animal favorito ?",
+        'respostas': ['Elefante', 'Leão', 'Cobra', 'Cabra'],
+      },
+      {
+        'texto': "Qual sua comida favorita ?",
+        'respostas': ['Japonesa', 'Chinesa', 'Brasileira', 'Árabe']
+      }
     ];
-    
+
+    List<Widget> respostas = [];
+
+    for (String textoResp in pergunta[_perguntaSelecionada]['respostas']) {
+      respostas.add(Resposta(textoResp,_responder));
+    }
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("QUIZ"),
+          title: Text("Perguntas"),
         ),
         body: Column(
           children: <Widget>[
-            Text(pergunta[0]),
-            RaisedButton(
-              child: Text("Resposta 1"),
-              onPressed: responder,
-            ),RaisedButton(
-              child: Text("Resposta 2"),
-              onPressed: responder,
-            ),RaisedButton(
-              child: Text("Resposta 3"),
-              onPressed: responder,
-            ),
+            Questao(pergunta[_perguntaSelecionada]['texto']),
+            ...respostas,
           ],
         ),
       ),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _perguntaAppState createState() {
+    return _perguntaAppState();
   }
 }
